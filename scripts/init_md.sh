9099cea875a00b4bfa4b3ea44839b82286fa0737
chhsiao <hsiao.chuanheng@gmail.com>
2014-07-14 00:49:56 +0800

    init_md

--- scripts/init_md.sh
+++ scripts/init_md.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+current_dir=`pwd`
+the_project=`basename ${current_dir}`
 rm README.md
-sed "s/\[PROJECT\]/${sub_project}/g" scripts/README.md.tmpl > README.md
+sed "s/\[PROJECT\]/${the_project}/g" scripts/README.md.tmpl > README.md
