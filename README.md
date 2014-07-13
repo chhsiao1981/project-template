454c57936d7b7764c2c6f176f87711d365cdae7c
chhsiao <hsiao.chuanheng@gmail.com>
2014-07-13 23:41:25 +0800

    init project-template

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ project-template
 ================
 
 A high-level project template including scripts of generating front-end/back-end sub-project template
+
+This intends to do the following:
+    1. init-backend.sh
+    2. init-frontend.sh
+
+This is mainly for speeding up developing my projects.
+All are welcome to fork and improve this template.
---
+++ scripts/init-backend.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "${BASH_ARGC}" != "1" ]
+then
+  echo "usage: init-backend.sh [sub-project]"
+  exit 0
+fi
+
+sub_project="${BASH_ARGV[0]}"
+scripts_backend=""
+current_dir=`pwd`
+
+mkdir -p "${sub_project}"
+cd "${sub_project}"
+
+git clone "${scripts_backend}" scripts
+
+scripts/init_dev.sh
+
+cd "${current_dir}"
+
+sed "s/\[PROJECT\]/${sub_project}/g" scripts/scripts_op.tmpl > scripts_op/${sub_project}.sh
+chmod 755 scripts_op/${sub_project}.sh
