f06f36f22ca08ac585dd3b4d12089b7f77a72ac9
chhsiao <hsiao.chuanheng@gmail.com>
2014-07-14 00:53:39 +0800

    scripts_op

--- scripts/init-frontend.sh
+++ scripts/init-frontend.sh
@@ -20,4 +20,5 @@ scripts/init_dev.sh
 cd "${current_dir}"
 
 sed "s/\[PROJECT\]/${sub_project}/g" scripts/scripts_op.tmpl > scripts_op/${sub_project}.sh
+mkdir -p scripts_op
 chmod 755 scripts_op/${sub_project}.sh
